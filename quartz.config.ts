d8847d327231e8e59be83583aa3b9772868eaf2e
jing-gu <jinggu.dev@gmail.com>
2025-06-28 14:37:27 -0400

    update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jinggu.dev",
+    baseUrl: "jinggu.dev/zettelkasten",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
